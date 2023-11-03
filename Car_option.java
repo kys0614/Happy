4c0fdad79825956f91559a093800a1579040577d
kys0614 <148218389+kys0614@users.noreply.github.com>
2023-11-03 18:58:05 +0900

    8일차 수업 내용 - 수정함

--- Car_option.java
+++ Car_option.java
@@ -1,5 +1,19 @@
 package Day8;
 
 public class Car_option {
+	int wheel_gold = 4; //멋진 휠
+	
+	/*
+	
+	//기본 생성자 : 호출하지 않아도 내부적으로 자동 호출되는 생성자
+	Car_option(){ // 클래스와 동일한 이름이 생성자이다.
+		this.wheel_gold = 0;
+		System.out.println("기본 생겅자로 출력됩니다.");
+	}
+	*/
+	S
+	void ride() { //반환 값이 없는 메서드 -> return X
+		System.out.println("달립니다.");
+	}
 
 }
--- day08_20231102.java
+++ day08_20231102.java
@@ -45,12 +45,90 @@ public class day08_20231102 {
 		car_.ride();
 		car_.ride();
 		car_.ride();
-
+		
+		//정적 멤버와 static (인스턴스/지역/정적) 변수를 구분한다.
+		System.out.println("Card.width="+Card.width);
+		System.out.println("Card.width="+Card.height);
+		
+		Card ca1 = new Card(); //객체 생성
+		ca1.kind = "Heart";
+		ca1.number = 7;
+		
+		Card ca2 = new Card(); //똑같은 설계도를 사용하는 객체2 생성
+		ca2.kind = "spade";
+		ca2.number = 4;
+		
+		//카드의 크기와 속성을 출력해보자.
+		//카드1에 대한 객체값 출력
+		System.out.println("Ca1은"+ca1.kind+","+ca1.number+"이며"+ca1.width+","+ca1.height);
+		//카드2에 대한 객체값 출력
+		System.out.println("Ca2은"+ca2.kind+","+ca2.number+"이며"+ca2.width+","+ca2.height);
+				
+		//메모리가 종료될 때까지 살아있다!
+		printHello1(); //객체도 아니고 클래스가 호출될 때 데이터 영역에 들어간다.
+		//메소드 문제 : 멤버 메소드
+		//1. 조거의 객체를 생성한다
+		jogger jo = new jogger();
+		//2. 조거의 이름(멤버변수)을 내 이름으로 선언한다.
+		jo.name = "김연수";
+		//3. 조거를 달리게 한다.
+		jo.run();
+		//4. 조거의 이름을 말ㄹ하게 한다.
+		jo.sayName();
+		
+		//메소드 문제 : static 메소드
+		//반환이 없는 메서드를 printgoodbye라고 만들고 잘가라고 이야기한다.
+		printgoodbye();
+		
+		int money = 10000;
+		Bus bus = new Bus();
+		bus.take(money);
+	}	
+	static void printHello1() { // 정적 메서드 (클래스가 실핼될 때 메모리에 메소드가 올라간다.)
+		System.out.println("실행해");
+		System.out.println("실행하고있구나");
+	}
+	
+	static void printgoodbye() {
+		System.out.println("잘가 안녕");
 	}
 
 }
 
 //라이브러리 클래스 : 실행용 클래스는 항상 1개 이면서 벌도로 만들어진 클래스
+class Bus{
+	void take(int m) {
+		
+		while(true) {
+			if(m<3000) {
+				System.out.println("교통카드를 충전하러 갑니다.");
+				return;
+			}
+			System.out.println("버스를 탑니다.");
+			m -= 1250;
+		}
+	}
+}
+
+class jogger{
+	String name;
+	void run() {
+		System.out.println("달리고 있어 런런런");
+	}
+	//
+	void sayName() {
+		System.out.println("내이름은"+name+"입니다.");
+	}
+}
+
+class Card{
+	
+	String kind; //모양
+	int number; //카드번호
+	static int width = 100; //넓이
+	static int height = 250; //높이
+}
+
 class Tv{
 	String color;
 	boolean power;
@@ -59,9 +137,11 @@ class Tv{
 	void channelUp() { ++channel; } // 소리가 켜진다.
 	void channelDown() { --channel; } // 소리가 작아진다.
 }
+
 class Car {
 	
 }
+
 class Tree {
 	String leaf = "green";
 }
